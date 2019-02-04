ebe971b9bc6e445afb94421823a7a2a72a196ede
Ken Alexandersson <ken.alexandersson@gmail.com>
2019-02-04 20:22:07 +0100

    Fixed a typo, removed fields no longer needed

--- items-webapi/src/main/java/org/microservices/itemswebapi/web/ItemInfoDto.java
+++ items-webapi/src/main/java/org/microservices/itemswebapi/web/ItemInfoDto.java
@@ -6,8 +6,6 @@ public class ItemInfoDto {
 
     private Item item;
     private List<Review> reviews;
-    private int itemPort;
-    private int reviewPort;
 
     public static ItemInfoDto of(Item item, List<Review> reviews) {
         return new ItemInfoDto(item, reviews);
@@ -16,8 +14,6 @@ public class ItemInfoDto {
     private ItemInfoDto(Item item, List<Review> reviews) {
         this.item = item;
         this.reviews = reviews;
-        this.itemPort = item.getPort();
-        this.reviewPort = !reviews.isEmpty() ? reviews.get(0).getPort() : 0;
     }
 
     public Item getItem() {
@@ -28,16 +24,9 @@ public class ItemInfoDto {
         return reviews;
     }
 
-    public int getItemPort() {
-        return itemPort;
-    }
-
-    public int getReviewPort() {
-        return reviewPort;
-    }
 
     @Override
     public String toString() {
-        return String.format("ItemInfoDto{item=%s, reviews=%s, itemPort=%d, reviewPort=%d}", item, reviews, itemPort, reviewPort);
+        return String.format("ItemInfoDto{item=%s, reviews=%s}", item, reviews);
     }
 }
